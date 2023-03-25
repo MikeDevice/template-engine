66e83eaa19b874fc346a728a4babd9ac66cfa058
Mikhail Vyrodov <me.vyrodov@ya.ru>
2023-03-25 19:58:56 +0300

    improve demo (#6)

--- docs/api/compile.js
+++ docs/api/compile.js
@@ -1,3 +1,4 @@
+const htmlBeautify = require('js-beautify').html;
 const { TemplateEngine } = require('../../dist');
 
 module.exports = (req, res) => {
@@ -5,7 +6,12 @@ module.exports = (req, res) => {
         const templateEngine = new TemplateEngine(req.body.code);
         const result = templateEngine.compile(req.body.ctx);
 
-        res.json({ code: result })
+        res.json({
+            code: htmlBeautify(
+                result,
+                { indent_size: 2, max_preserve_newlines: 0 }
+            )
+        })
     } catch (err) {
         res.status(500).json({ error: `${err.name}: ${err.message}` })
     }
